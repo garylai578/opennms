c54ff38294aa3d72f88e790600e9fcafa372dbd3
laiguanhui <garylai414@126.com>
2016-05-18 09:51:19 +0800

    交换机流量监控由平均流量修改为累计流量（小时）

--- opennms-webapp/src/main/java/org/opennms/web/abcbank/StartSwitcherMonitorServlet.java
+++ opennms-webapp/src/main/java/org/opennms/web/abcbank/StartSwitcherMonitorServlet.java
@@ -30,7 +30,6 @@ public class StartSwitcherMonitorServlet extends HttpServlet {
     private Map resultMap = new HashMap();
     private String hour1,hour2;
     private String day1, day2;
-    private int t = 1;
 
     public void init(){
         flag = true;
@@ -49,19 +48,12 @@ public class StartSwitcherMonitorServlet extends HttpServlet {
                 if(!day1.equals(day2)){
                     day1 = day2;
                     hour1 = hour2;
-                    t = 1;
-                }
-                if(hour2.equals(hour1)) {   //同一个小时内的流量进行累计
-                    t++;
-                }else{
-                    hour1 = hour2;
-                    t = 1;
                 }
             }
         };
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-        // 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
-        service.scheduleAtFixedRate(runnable, 0, 600, TimeUnit.SECONDS);
+        // 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间，考虑到交换机流量最大记录4Gbits，因此每分钟获取一次流量值
+        service.scheduleAtFixedRate(runnable, 0, 60, TimeUnit.SECONDS);
     }
 
     private void startSwitcherMonitor(){
@@ -79,15 +71,14 @@ public class StartSwitcherMonitorServlet extends HttpServlet {
                 BankLogWriter.getSingle().writeLog("监控交换机的流量：" + sw.getIp());
 //                BankLogWriter.getSingle().writeLog("时间hour1：" + hour1 + ", hour2:" + hour2 + "; day1:" + day1 + ", day2: " + day2);
 
-                Flow flow = new Flow(sw.getIp());
+                SwitcherFlowMonitor flow = new SwitcherFlowMonitor(sw.getIp());
                 flow.setOcter("abc123");
-                long inFlowValue = flow.calcFlowValue(inFlowOidGroup);
-                long outFlowValue = flow.calcFlowValue(outFlowOidGroup);
-                BankLogWriter.getSingle().writeLog("inflow：" + inFlowValue + ", outFlow:" + outFlowValue);
+                long newInFlow = flow.getFlowValue(inFlowOidGroup);
+                long newOutFlow = flow.getFlowValue(outFlowOidGroup);
 
                 //如果是新添加的交换机，则先加入到Map中
                 if(!resultMap.containsKey(sw.getIp())){
-                    String resultValue = 0 + "\t" + 0;
+                    String resultValue = 0 + "\t" + 0 + "\t" + 0 + "\t" + 0;
                     resultMap.put(sw.getIp(), resultValue);
                 }
 
@@ -99,13 +90,49 @@ public class StartSwitcherMonitorServlet extends HttpServlet {
                 if(hour2.equals(hour1)){   //同一个小时内的流量进行累计
                     String resultValue = (String)resultMap.get(sw.getIp());
                     String[] values = resultValue.split("\t");
-                    long inFlow = Long.parseLong(values[0]) + inFlowValue;
-                    long outFlow = Long.parseLong(values[1]) + outFlowValue;
-                    resultMap.put(sw.getIp(), inFlow + "\t" + outFlow);
-                    insertDB(sw.getIp());   //进行实时结果的显示
-                }else{      //对一个小时内的流量进行计算后插入数据库，并替换原值
-                    insertDB(sw.getIp());
-                    resultMap.put(sw.getIp(), inFlowValue + "\t" + outFlowValue);
+                    long oldInFlow = Long.parseLong(values[0]);
+                    long oldOutFlow = Long.parseLong(values[1]);
+                    long inFlow = Long.parseLong(values[2]);
+                    long outFlow = Long.parseLong(values[3]);
+                    long subInFlow = newInFlow - oldInFlow;
+                    long subOutFlow = newOutFlow - oldOutFlow;
+                    /*
+                     * 端口流量值为无符号32位，超出后就归0，所以如果两次取值差值为负，
+                     * 必然出现一次归0的情况，由于单个端口的流量不可能超过每5秒1*2^32字节
+                     */
+                    if(subInFlow < 0 ){
+                        // 因为端口流量为无符号32位，所以最大值是有符号32位的两倍
+                        subInFlow += 2L * Integer.MAX_VALUE;
+                    }
+                    if(subOutFlow < 0 )
+                        subOutFlow += 2L * Integer.MAX_VALUE;
+                    //累计流量
+                    inFlow += subInFlow;
+                    outFlow += subOutFlow;
+                    resultMap.put(sw.getIp(), newInFlow + "\t" + newOutFlow + "\t" + inFlow + "\t" + outFlow);
+                    BankLogWriter.getSingle().writeLog("newInFlow：" + newInFlow + ", oldInFlow:" + oldInFlow + ", newOutFlow:" + newOutFlow + ", oldOutFlow:" + oldOutFlow);
+                    BankLogWriter.getSingle().writeLog("subInFlow：" + subInFlow + ", subOutFlow:" + subOutFlow + ", inFlow:" + inFlow + ", outFlow:" + outFlow);
+
+                    //插入数据库
+                    String oldValue = operator.getColunm(sw.getIp(), "flow");
+                    String[] oldSplit = oldValue.split(",|/t");
+
+                    oldSplit[Integer.parseInt(hour1) - 1] = inFlow + "";
+                    oldSplit[Integer.parseInt(hour1) + 24 - 1] = outFlow + "";
+
+                    String newString ="";
+                    for(int j=0; j < oldSplit.length; ++j){
+                        if(j == 23)
+                            newString += oldSplit[j] + "/t";
+                        else
+                            newString += oldSplit[j] + ",";
+                    }
+                    newString = newString.substring(0, newString.length() -1);
+                    operator.update(sw.getIp(), "flow", "'" + newString + "'" );
+
+                }else{
+                    resultMap.put(sw.getIp(), newInFlow + "\t" + newOutFlow + "\t" + 0 + "\t" + 0);
+                    hour1 = hour2;
                 }
             }
         } catch (SQLException e) {
@@ -114,33 +141,6 @@ public class StartSwitcherMonitorServlet extends HttpServlet {
 
     }
 
-    private void insertDB(String ip) throws SQLException {
-        SwitcherStatsOperator operator = new SwitcherStatsOperator();
-        String resultValue = (String)resultMap.get(ip);
-        String[] values = resultValue.split("\t");
-        long inFlow = Long.parseLong(values[0]);
-        long outFlow = Long.parseLong(values[1]);
-        BankLogWriter.getSingle().writeLog(hour1 + "点的流入：" + inFlow + "bit, 流出：" + outFlow + "bit");
-
-        // byte换算成KB
-        long hourInFlow = (long) (inFlow / (1024 * 8.0) / t);
-        long hourOutFlow = (long) (outFlow / (1024 * 8.0) / t);
-        String oldValue = operator.getColunm(ip, "flow");
-        String[] oldSplit = oldValue.split(",|/t");
-
-        oldSplit[Integer.parseInt(hour1) - 1] = hourInFlow + "";
-        oldSplit[Integer.parseInt(hour1) + 24 - 1] = hourOutFlow + "";
-
-        String newString ="";
-        for(int j=0; j < oldSplit.length; ++j){
-            if(j == 23)
-                newString += oldSplit[j] + "/t";
-            else
-                newString += oldSplit[j] + ",";
-        }
-        newString = newString.substring(0, newString.length() -1);
-        operator.update(ip, "flow", "'" + newString + "'" );
-    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if(!flag) {
