5fff8091c017933778e4ceae14bb1ec2a385ec71
laiguanhui <garylai414@126.com>
2016-06-20 16:43:46 +0800

    将数据库所有内容导出到excel

--- opennms-webapp/src/main/java/org/opennms/web/abcbank/ExportIPSegmentServlet.java
+++ opennms-webapp/src/main/java/org/opennms/web/abcbank/ExportIPSegmentServlet.java
@@ -3,14 +3,18 @@ package org.opennms.web.abcbank;
 import org.opennms.core.bank.BankLogWriter;
 import org.opennms.core.bank.ExportExcel;
 import org.opennms.core.bank.IPSegment;
+import org.opennms.core.bank.IPSegmentOperater;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -21,42 +25,42 @@ public class ExportIPSegmentServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userId = request.getRemoteUser();
-        String tmp = request.getParameter("rows");
-        int rows = Integer.parseInt(tmp);
-        List<IPSegment> dataset = new ArrayList<IPSegment>();
-        String ipSeg = "";
-        response.setContentType("octets/stream");
-        response.addHeader("Content-Disposition", "attachment;filename=result.xls");
-        ExportExcel<IPSegment> ex = new ExportExcel<IPSegment>();
-        String[] headers = { "id", "所属IP段", "网关", "掩码", "开始IP", "结束IP", "网点名称", "网点类型", "启用日期", "使用情况", "备注"};
-        OutputStream out = response.getOutputStream();
 
-        for(int i = 0; i < rows; ++i){
-            IPSegment line = new IPSegment();
-            line.setId(request.getParameter("id-"+i));
-            line.setSegment(request.getParameter("ipSeg-"+i));
-            line.setGateway(request.getParameter("gateway-"+i));
-            line.setMask(request.getParameter("mask-"+i));
-            line.setStartIP(request.getParameter("startIP-"+i));
-            line.setEndIP(request.getParameter("endIP-"+i));
-            line.setBankname(request.getParameter("bankname-"+i));
-            line.setBanktype(request.getParameter("banktype-"+i));
-            line.setCreateTime(request.getParameter("createdate-"+i));
-            line.setState(request.getParameter("state-"+i));
-            line.setComment(request.getParameter("comment-"+i));
-            if(i == 0)
-                ipSeg = line.getSegment();
-            if(ipSeg == line.getSegment())
-                dataset.add(line);
-            else{
-                ex.exportExcel(ipSeg, headers, dataset, out);
-                ipSeg = line.getSegment();
-                dataset.clear();
+        try {
+            IPSegmentOperater op = new IPSegmentOperater();
+            IPSegment[] ips = op.selectAll("");
+            List<IPSegment> ipSegmentList = new ArrayList<IPSegment>();
+            for(IPSegment ip : ips)
+                ipSegmentList.add(ip);
+            Collections.sort(ipSegmentList, IPSegment.IPComparator);
+
+            List<IPSegment> dataset = new ArrayList<IPSegment>();
+            String ipSeg = "";
+            response.setContentType("octets/stream");
+            response.addHeader("Content-Disposition", "attachment;filename=result.xls");
+            ExportExcel<IPSegment> ex = new ExportExcel<IPSegment>();
+            String[] headers = { "id", "所属IP段", "网关", "掩码", "开始IP", "结束IP", "网点名称", "网点类型", "启用日期", "使用情况", "备注"};
+            OutputStream out = response.getOutputStream();
+
+            for(IPSegment line : ipSegmentList){
+                if("".equals(ipSeg))
+                    ipSeg = line.getSegment();
+                if(ipSeg == line.getSegment())
+                    dataset.add(line);
+                else{
+                    ex.exportExcel(ipSeg, headers, dataset, out);
+                    ipSeg = line.getSegment();
+                    dataset.clear();
+                }
             }
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-        out.close();
-
 
         BankLogWriter.getSingle().writeLog("用户[" + userId +"]导出IP地址段分配报表");
     }
