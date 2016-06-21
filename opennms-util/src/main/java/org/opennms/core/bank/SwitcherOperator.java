3cbd37a54fd60153ac56a026b645996be95df127
laiguanhui <garylai414@126.com>
2016-06-21 15:36:35 +0800

    交换机配置页面的搜索功能修改为组合搜索

--- opennms-util/src/main/java/org/opennms/core/bank/SwitcherOperator.java
+++ opennms-util/src/main/java/org/opennms/core/bank/SwitcherOperator.java
@@ -7,6 +7,8 @@ import org.opennms.core.utils.DBUtils;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by laiguanhui on 2016/3/30.
@@ -123,6 +125,42 @@ public class SwitcherOperator {
     }
 
     /**
+     * search the cols with key
+     * @param colsAndValues columns and the searching values
+     * @return the searching result
+     * @throws SQLException
+     */
+    public Switcher[] andSelect(Map<String, String> colsAndValues) throws SQLException  {
+        Switcher[] result = null;
+        try {
+            Connection conn = Vault.getDbConnection();
+            d.watch(conn);
+            Statement stmt = conn.createStatement();
+            d.watch(stmt);
+            String sql = "SELECT * FROM switcher WHERE ";
+            Set<String> cols = colsAndValues.keySet();
+            for(String col : cols){
+                String value = colsAndValues.get(col);
+                sql += col + " LIKE '%"+ value + "%' and ";
+            }
+
+            if(cols.size() == 0)
+                sql = "SELECT * FROM switcher";
+            else
+                sql = sql.substring(0, sql.length()-4);
+
+            log.debug("search sql: " + sql);
+            ResultSet rs = stmt.executeQuery(sql);
+            d.watch(rs);
+            result = rs2Switcher(rs);
+        } finally {
+            d.cleanUp();
+        }
+
+        return result;
+    }
+
+    /**
      * Delete a record from table switcher according to the id
      *
      * @param id
@@ -174,4 +212,5 @@ public class SwitcherOperator {
         result = list.toArray(new Switcher[list.size()]);
         return result;
     }
+
 }
