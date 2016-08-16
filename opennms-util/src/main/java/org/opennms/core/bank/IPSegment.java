806f44d901a84cff9d72399b25aacc9a85fe7b81
laiguanhui <garylai414@126.com>
2016-08-16 17:08:56 +0800

    如果为空则插入数据库的是''

--- opennms-util/src/main/java/org/opennms/core/bank/IPSegment.java
+++ opennms-util/src/main/java/org/opennms/core/bank/IPSegment.java
@@ -126,7 +126,15 @@ public class IPSegment {
     }
 
     public String toInsertValues() {
-        return  "'" + segment + "', '" + gateway + "', '" + mask + "', '" + startIP + "', '" + endIP + "', '"  + bankname + "', '"  + createTime + "', '"   + banktype + "', '"  + state + "', '"  + comment +"'";
+        String[] colsString = {segment, gateway, mask, startIP, endIP, bankname, createTime, banktype, state, comment};
+        String value = "";
+        for (String col: colsString) {
+            if(col == null)
+                value += ", ''";
+            else
+                value += ", '" + col + "'";
+        }
+        return  value;
 
     }
 
